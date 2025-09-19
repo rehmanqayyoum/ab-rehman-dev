a064be3ed0dce3abc6a9ff262b064718db3d5510
Abdul Rehman <77438767+rehmanqayyoum@users.noreply.github.com>
2025-09-19 19:04:36 +0500

    add toast

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import Navbar from "./components/Navbar";
 import "./index.css"
 import Home from "./pages/Home";
 import FixTimelines from "./components/FixTimelines";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
 
 export default function App() {
   return (
@@ -11,6 +14,16 @@ export default function App() {
       <Home />
 
       <FixTimelines />
+
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        theme="colored"
+      />
     </main>
   );
 }
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,19 +1,43 @@
 import React, { useRef, useState } from 'react';
 import emailjs from "@emailjs/browser";
 import { Button } from './ui/button';
+import { toast } from "react-toastify";
 
 const ContactForm = () => {
     const formRef = useRef(null);
     const [loading, setLoading] = useState(false);
-    const [status, setStatus] = useState(null); // "success" | "error" | null
+    // const [status, setStatus] = useState(null); // "success" | "error" | null
+
+    // const sendEmail = async (e) => {
+    //     e.preventDefault();
+    //     setStatus(null);
+
+    //     const hp = new FormData(formRef.current).get("website");
+    //     if (hp) return;
+
+    //     setLoading(true);
+    //     try {
+    //         await emailjs.sendForm(
+    //             import.meta.env.VITE_EMAILJS_SERVICE_ID,
+    //             import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+    //             formRef.current,
+    //             { publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY }
+    //         );
+
+    //         setStatus("success");
+    //         formRef.current.reset();
+    //     } catch (err) {
+    //         console.error(err);
+    //         setStatus("error");
+    //     } finally {
+    //         setLoading(false);
+    //     }
+    // };
 
     const sendEmail = async (e) => {
         e.preventDefault();
-        setStatus(null);
-
-        // Honeypot check (hidden field)
         const hp = new FormData(formRef.current).get("website");
-        if (hp) return; // bot detected
+        if (hp) return;
 
         setLoading(true);
         try {
@@ -24,19 +48,18 @@ const ContactForm = () => {
                 { publicKey: import.meta.env.VITE_EMAILJS_PUBLIC_KEY }
             );
 
-            setStatus("success");
+            toast.success("Message sent successfully!");
             formRef.current.reset();
         } catch (err) {
             console.error(err);
-            setStatus("error");
+            toast.error("Something went wrong. Please try again");
         } finally {
             setLoading(false);
         }
     };
 
-
     return (
-        <form className=" space-y-5 p-4 sm:p-6 md:p-8 rounded-2xl border bg-gray-50 shadow-sm w-full max-w-lg" ref={formRef} onSubmit={sendEmail}>
+        <form className=" space-y-5 p-4 sm:p-6 md:p-8 rounded-2xl border bg-gray-50 shadow-sm w-full max-w-lg md:mr-12" ref={formRef} onSubmit={sendEmail} noValidate>
             <div>
                 <label className="block mb-2 text-sm sm:text-base md:text-[16px] font-medium text-muted-foreground">Your Name</label>
                 <input
@@ -51,7 +74,7 @@ const ContactForm = () => {
                 <label className="block mb-2 text-sm sm:text-base md:text-[16px] font-medium text-muted-foreground">Your Email</label>
                 <input
                     type="email"
-                    name="user_email" /* must match EmailJS template */
+                    name="user_email"
                     required
                     className="w-full rounded-lg border border-gray-300 px-3 sm:px-4 py-2.5 sm:py-3 font-sans text-sm sm:text-base md:text-[16px] focus:border-primary focus:ring-2 focus:ring-primary/40 focus:outline-none transition"
                     placeholder="Enter your email"
@@ -61,7 +84,7 @@ const ContactForm = () => {
                 <label className="block mb-2 text-sm sm:text-base md:text-[16px] font-medium text-muted-foreground">Message</label>
                 <textarea
                     rows={4}
-                    name="message" /* must match EmailJS template */
+                    name="message"
                     required
                     className="w-full rounded-lg border border-gray-300 px-3 sm:px-4 py-2.5 sm:py-3 font-sans text-sm sm:text-base md:text-[16px] focus:border-primary focus:ring-2 focus:ring-primary/40 focus:outline-none transition"
                     placeholder="Write your message..."
@@ -79,12 +102,12 @@ const ContactForm = () => {
                 {loading ? "Sending..." : "Send Message"}
             </Button>
 
-            {status === "success" && (
+            {/* {status === "success" && (
                 <p className="text-sm text-green-600">Message sent. I’ll get back to you soon!</p>
             )}
             {status === "error" && (
                 <p className="text-sm text-red-600">Something went wrong. Please try again.</p>
-            )}
+            )} */}
         </form>
     )
 }
